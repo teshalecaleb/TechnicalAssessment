351a78fc59d2a5f99d8320e078ed20e4b3fd2455
teshalecaleb <teshalecaleb@gmail.com>
2023-01-31 16:13:03 +0300

    play card modified

--- assesment_project/lib/pages/home/playlist_card.dart
+++ assesment_project/lib/pages/home/playlist_card.dart
@@ -15,20 +15,27 @@ class PlayListCard extends StatelessWidget {
     return Card(
       margin: EdgeInsets.symmetric(horizontal: 15.w, vertical: 32.h),
       color: Colors.transparent,
-      //shadowColor: Colors.white,
-      elevation: 0.h,
+      shadowColor: Color(0xff504c57),
+      surfaceTintColor: Color(0xfff28262C33),
+      elevation: 0.2.h,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(32.r),
         side: BorderSide(
           width: 1.w,
-          color: Color(0xff645f6d),
+          color: Colors.black,
         ),
       ),
       child: Container(
         height: 403.h,
+        width: double.infinity,
+        padding: EdgeInsets.only(
+          top: 12.h,
+          left: 12.w,
+          right: 12.w,
+        ),
         //width: double.maxFinite,
 
-        //color: Colors.green,
+        // color: Colors.green,
         // decoration: BoxDecoration(
         //   borderRadius: BorderRadius.circular(32.r),
         //   gradient: LinearGradient(
@@ -41,15 +48,22 @@ class PlayListCard extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Container(
-              height: 288.h,
+            // Container(
+            //   color: Colors.green,
+            //   height: 288.h,
+            //   //width: 321.w,
+            //   width: double.infinity,
+            //   margin: EdgeInsets.only(top: 12.h, left: 12.w, right: 12.w),
+            //   child:
+            Image.asset(
+              'assets/images/cards/card1.png',
               width: 321.w,
-              margin: EdgeInsets.only(top: 12.h, left: 12.w, right: 12.w),
-              child: Image.asset("assets/images/cards/card1.png"),
+              height: 288.h,
             ),
+            // ),
             Row(
               children: [
-                // Text("data"),
+                //Text("data"),
               ],
             )
           ],
